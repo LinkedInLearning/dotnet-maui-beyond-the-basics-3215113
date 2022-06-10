57197cab262f39ac1036ec6f0c5c796af221850d
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2022-06-09 20:31:13 -0700

    end of 03_04

---
+++ MauiApp/MauiApp/Extensions/XmlLoaderExtension.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MauiBeyond.Extensions
+{
+    public class XmlLoaderExtension : IMarkupExtension<string>
+    {
+        public string TargetNode { get; set; }
+        public string ResourceFile { get; set; }
+
+        public string ProvideValue(IServiceProvider serviceProvider)
+        {
+            string returnValue = string.Empty;
+            if (!string.IsNullOrEmpty(ResourceFile) && !string.IsNullOrEmpty(TargetNode))
+            {
+                var assembly = IntrospectionExtensions.GetTypeInfo(typeof(XmlLoaderExtension)).Assembly;
+                Stream stream = assembly.GetManifestResourceStream(ResourceFile);
+                string text = "";
+                using (var reader = new System.IO.StreamReader(stream))
+                {
+                    text = reader.ReadToEnd();
+                }
+                var xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(text);
+                var nodes = xmlDoc.GetElementsByTagName(TargetNode);
+                if (nodes != null && nodes.Count > 0)
+                {
+                    returnValue = nodes[0].InnerText;
+                }
+            }
+            return returnValue;
+
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return (this as IMarkupExtension<string>).ProvideValue(serviceProvider);
+        }
+    }
+}
