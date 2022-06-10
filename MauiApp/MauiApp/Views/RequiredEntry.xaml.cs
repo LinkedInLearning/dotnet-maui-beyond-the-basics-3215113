32f967c32165642e878f22d0ba4201ebe518d62e
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2022-06-10 16:14:59 -0700

    beginning of chapter 03_07

--- MauiApp/MauiApp/Views/RequiredEntry.xaml.cs
+++ MauiApp/MauiApp/Views/RequiredEntry.xaml.cs
@@ -5,12 +5,20 @@ public partial class RequiredEntry : ContentView
 	public RequiredEntry()
 	{
 		InitializeComponent();
-	}
+        HasTextChanged(this, string.Empty, string.Empty);
 
-    public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(RequiredEntry), string.Empty, BindingMode.TwoWay);
+    }
+
+    public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(RequiredEntry), string.Empty, BindingMode.TwoWay, null, HasTextChanged);
     public string Text
     {
         get => (string)GetValue(TextProperty);
         set => SetValue(TextProperty, value);
+    }
+
+    private static void HasTextChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var newText = newValue as string;
+        VisualStateManager.GoToState(((RequiredEntry)bindable).mainFrame, string.IsNullOrEmpty(newText) ? "NoText" : "HasText");
     }
 }
