0e7b8099e27034c4230208f318c67de8758fcd18
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2022-06-11 19:21:31 -0700

    start of 04_02

--- MauiApp/MauiApp/ViewModels/MainViewModel.cs
+++ MauiApp/MauiApp/ViewModels/MainViewModel.cs
@@ -1,4 +1,4 @@
-﻿using MauiBeyond.Models;
+﻿
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 
@@ -9,57 +9,6 @@ namespace MauiBeyond.ViewModels
 
         public MainViewModel()
         {
-            Addresses.Add(new Address
-            {
-                Address1 = "221b Baker Street",
-                City = "London",
-                PostalCode = "NW1 6XE"
-            });
-            Addresses.Add(new Address
-            {
-                Address1 = "704 Hauser St.",
-                City = "New York",
-                State = "NY"
-            });
-            Addresses.Add(new Address
-            {
-                Address1 = "322 Maple St.",
-                City = "Mayberry",
-                State = "NC"
-            });
         }
-
-        private ObservableCollection<Address> _Addresses = new ObservableCollection<Address>();
-        public ObservableCollection<Address> Addresses
-        {
-            get => _Addresses;
-            set
-            {
-                if (_Addresses != value)
-                {
-                    _Addresses = value;
-                    OnPropertyChanged(nameof(Addresses));
-                }
-            }
-        }
-
-        public bool CanDeleteAddresses
-        {
-            get => Addresses.Count > 1;
-        }
-
-        private ICommand _DeleteAddressCommand;
-        public ICommand DeleteAddressCommand
-        {
-            get { return _DeleteAddressCommand ??= new Command<Address>((address) => DeleteAddress(address)); }
-        }
-
-        private void DeleteAddress(Address address)
-        {
-            if (Addresses.Contains(address) && CanDeleteAddresses)
-            {
-                Addresses.Remove(address);
-            }
-        }
-    }
+   }
 }
