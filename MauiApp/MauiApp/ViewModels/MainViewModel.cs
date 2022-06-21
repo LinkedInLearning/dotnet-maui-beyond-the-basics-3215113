1407428af6db073fc6e0431f148901b4a670e153
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2022-06-20 19:30:07 -0700

    Start of 05_04

--- MauiApp/MauiApp/ViewModels/MainViewModel.cs
+++ MauiApp/MauiApp/ViewModels/MainViewModel.cs
@@ -1,5 +1,4 @@
-﻿using MauiBeyond.Models;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 namespace MauiBeyond.ViewModels
@@ -9,32 +8,6 @@ namespace MauiBeyond.ViewModels
 
         public MainViewModel()
         {
-            LoadAddress();
-        }
-
-        private Address _Address;
-        public Address Address
-        {
-            get => _Address;
-            private set
-            {
-                if (_Address != value)
-                {
-                    _Address = value;
-                    OnPropertyChanged(nameof(Address));
-                }
-            }
-        }
-
-        private ICommand _LoadAddressCommand;
-        public ICommand LoadAddressCommand
-        {
-            get { return _LoadAddressCommand ??= new Command(() => LoadAddress()); }
-        }
-
-        public void LoadAddress()
-        {
-            Address = new Address();
         }
     }
 }
