133982d649d77ce8e0851c8341e5ca03442e5bab
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2022-06-20 19:33:42 -0700

    end of 05_04

---
+++ MauiApp/MauiApp/Animations/Animations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBeyond.Animations
+{
+    public static class Animations
+    {
+        public static Task<bool> PopAnimation(this VisualElement self, uint duration = 1500, Easing easing = null)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            var startHeight = self.Height;
+            var startWidth = self.Width;
+
+            if (easing == null) easing = Easing.Linear;
+
+        }
+    }
+}
