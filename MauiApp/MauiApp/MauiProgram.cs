b58fd3431defc2ae79283383d6b4d4ccd688743b
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2022-06-21 18:35:06 -0700

    End of 05_06

--- MauiApp/MauiApp/MauiProgram.cs
+++ MauiApp/MauiApp/MauiProgram.cs
@@ -20,6 +20,29 @@ public static class MauiProgram
 
 		builder.Services.AddTransient<MainViewModel>();
 
+#if ANDROID
+        Microsoft.Maui.Handlers.ImageHandler.Mapper.ModifyMapping(nameof(Image.IsVisible), (handler, view, action) =>
+        {
+            var myView = handler.PlatformView;
+
+            if (myView.Visibility == Android.Views.ViewStates.Visible)
+            {
+                myView.Visibility = Android.Views.ViewStates.Gone;
+            }
+            else
+            {
+                var width = myView.Width > 0 ? myView.Width / 2 : 500;
+                var height = myView.Height > 0 ? myView.Height / 2 : 300;
+
+                float finalRadius = (float)Math.Sqrt(Math.Pow(width, 2) + Math.Pow(height, 2));
+
+                var anim = Android.Views.ViewAnimationUtils.CreateCircularReveal(myView, width, height, 0f, finalRadius);
+                myView.Visibility = Android.Views.ViewStates.Visible;
+                anim.Start();
+            }
+        });
+#endif
+
         return builder.Build();
 	}
 }
