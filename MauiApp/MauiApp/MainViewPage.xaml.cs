0eb6d50743d05e785434d197e9738a4f2f2bfbc4
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2022-06-22 21:25:54 -0700

    start of 05_07

--- MauiApp/MauiApp/MainViewPage.xaml.cs
+++ MauiApp/MauiApp/MainViewPage.xaml.cs
@@ -10,41 +10,5 @@ public partial class MainViewPage : ContentPage
         BindingContext = mainViewModel;
 
         InitializeComponent();
-
-        address1.Focused += Address1_Focused;
-        address2.Focused += Address2_Focused;
-        city.Focused += City_Focused;
-        state.Focused += State_Focused;
-        postalCode.Focused += PostalCode_Focused;
-    }
-
-    private void PostalCode_Focused(object sender, FocusEventArgs e)
-    {
-        MoveIndicator(MOVE_DISTANCE * 4);
-    }
-
-    private void State_Focused(object sender, FocusEventArgs e)
-    {
-        MoveIndicator(MOVE_DISTANCE * 3);
-    }
-
-    private void City_Focused(object sender, FocusEventArgs e)
-    {
-        MoveIndicator(MOVE_DISTANCE * 2);
-    }
-
-    private void Address2_Focused(object sender, FocusEventArgs e)
-    {
-        MoveIndicator(MOVE_DISTANCE * 1);
-    }
-
-    private void Address1_Focused(object sender, FocusEventArgs e)
-    {
-        MoveIndicator(MOVE_DISTANCE * 0);
-    }
-
-    private void MoveIndicator(int distance)
-    {
-        focusIndicator.TranslateTo(0, distance, 400, Easing.BounceOut);
     }
 }
---
+++ MauiApp/MauiApp/Triggers/TappedAnimationTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBeyond.Triggers
+{
+    public class TappedAnimationTrigger : TriggerAction<Button>
+    {
+        protected override void Invoke(Button sender)
+        {
+   
+        }
+    }
+}
