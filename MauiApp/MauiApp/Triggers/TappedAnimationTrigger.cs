002ca8e02826a8c3947592972fd87d0979f7847a
Kevin E. Ford <4380198+Bowman74@users.noreply.github.com>
2022-07-28 16:18:46 -0700

    Changes for final challenge code

--- MauiApp/MauiApp/Triggers/TappedAnimationTrigger.cs
+++ MauiApp/MauiApp/Triggers/TappedAnimationTrigger.cs
@@ -8,9 +8,31 @@ namespace MauiBeyond.Triggers
 {
     public class TappedAnimationTrigger : TriggerAction<Button>
     {
-        protected override void Invoke(Button sender)
+        protected override async void Invoke(Button sender)
         {
-   
+            using (var downAnimation = new Animation(v => sender.Shadow.Offset = new Point(5 - (10 * v), 5 - (10 * v)), 0, 1, Easing.Linear))
+            {
+                var tcs1 = new TaskCompletionSource<bool>();
+
+                downAnimation.Commit(sender, "down", 10, 200, null, (a, b) => tcs1.SetResult(true));
+
+                await Task.WhenAll(
+                    sender.TranslateTo(10, 10, 200),
+                    tcs1.Task
+                    );
+            }
+
+            using (var upAnimation = new Animation(v => sender.Shadow.Offset = new Point(5 + (10 * v), 5 + (10 * v)), 0, 1, Easing.Linear))
+            {
+                var tcs2 = new TaskCompletionSource<bool>();
+
+                upAnimation.Commit(sender, "up", 10, 200, null, (a, b) => tcs2.SetResult(true));
+
+                await Task.WhenAll(
+                    sender.TranslateTo(0, 0, 200),
+                    tcs2.Task
+                    );
+            }
         }
     }
 }
